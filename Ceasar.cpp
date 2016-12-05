1483114e7e06dd49850acea067ece218379b80ba
Busiedblowfish <oobey1@aol.com>
2016-12-05 05:57:31 -0500

    Works <= 5 letters

--- Ceasar.cpp
+++ Ceasar.cpp
@@ -9,69 +9,168 @@ using namespace std;
 
 /* Fucntion prototype */
 void mainMenu(void);
-string plainText();
+string userInput();
 string leftSegment(const string&);
 string rightSegment(const string&);
 string leftCipher(string&, const int);
 string rightCipher(string&, const int);
 string interLeavedCipher(const string&, const string&);
 string ceasarCipher(string&, const int);
+string ceasarDecipher(string&, const int);
+string leftDecipher(string&, const int);
+string rightDecipher(string&, const int);
 
 
 
 int main()
 {
-    string plaintext;   //The plaintext to be encrypted
     string leftSeg;     //The left segment of the plaintext
     string rightSeg;    //The right segment of the plaintext
     string leftCi;
     string rightCi;
-    int option = 0;     //Option for the main menu
-    int key = 0;        //Number of shift positions
+    unsigned int option = 0;     //Option for the main menu
+    unsigned int k1 = 0;        //Number of shift positions, k1
+    unsigned int k2 = 0;        //Number of shift positions, k2
+    unsigned int k3 = 0;        //Number of shift positions, k3
+
+
     mainMenu();
 
     cout << "\nEnter your option: ";
     cin >> option;
     cin.ignore();
-    while(1)
+    if(option >= 1  && option <= 3)
     {
-        //Encrypt a plaintext
-        if(option == 1)
-        {
-            plaintext = plainText();
-            leftSeg = leftSegment(plaintext);
-            rightSeg = rightSegment(plaintext);
-            cout << "\nLeft Segment: " << leftSeg << "\n";
-            cout << "Right Segment: " << rightSeg << "\n";
-
-            leftCi = leftCipher(leftSeg, 4);
-            rightCi = rightCipher(rightSeg, 4);
-
-            /* Interleave the each cipher text from leftCi and rightCi*/
-            string interCipher;
-            interCipher = interLeavedCipher(leftCi, rightCi);
-            /* Encrypt the interleaved cipher text using k3 */
-            string finalCipher;
-            finalCipher = ceasarCipher(interCipher, 4);
-        }
-
-        //Decrypt a cipher text
-        else if(option == 2)
-        {
-            break;
-        }
-        else if(option == 3)
+        if(option == 3)
         {
             cout << "Goodbye!\n";
-            break;
+            exit(1);
         }
         else
         {
-            cout << "Invalid option, try again\n";
+            while(1)
+            {
+                //Encrypt a plaintext
+                if(option == 1 && cin.good())
+                {
+                    string plaintext;   //The plaintext to be encrypted
+
+                    cout << "Enter the plaintext to encrypt: ";
+                    plaintext = userInput();
+                    cout << "Key ranges from [1 - 26]";
+                    cout << "\nEnter key[1]: ";
+                    cin >> k1;
+                    cout << "\nEnter key[2]: ";
+                    cin >> k2;
+                    cout << "\nEnter key[3]: ";
+                    cin >> k3;
+                    while(1)
+                    {
+                        if((k1 >= 1 && k1 <= 25) && (k2 >= 1 && k2 <= 25) && (k3 >= 1 && k3 <= 25))
+                        {
+                            leftSeg = leftSegment(plaintext);
+                            rightSeg = rightSegment(plaintext);
+                            cout << "\nLeft Segment: " << leftSeg << "\n";
+                            cout << "Right Segment: " << rightSeg << "\n";
+
+                            leftCi = leftCipher(leftSeg, k1);
+                            rightCi = rightCipher(rightSeg, k2);
+
+                            /* Interleave the each cipher text from leftCi and rightCi*/
+                            string interCipher;
+                            interCipher = interLeavedCipher(leftCi, rightCi);
+                            /* Encrypt the interleaved cipher text using k3 */
+                            string finalCipher;
+                            finalCipher = ceasarCipher(interCipher, k3);
+                            cout << "\nFinal Ciphertext : " << finalCipher << "\n";
+                            break;  //Return to outer loop
+                        }
+                        else
+                        {
+                            cout << "\nInvalid input(s)\n";
+                            break;
+                        }
+                    }
+                }
+
+                //Decrypt a cipher text
+                else if(option == 2 && cin.good())
+                {
+                    string finalCiphertext;   //The plaintext to be encrypted
+
+                    cout << "Enter the ciphertext to decrypt: ";
+                    finalCiphertext = userInput();
+                    cout << "Key ranges from [1 - 26]";
+                    cout << "\nEnter key[1]: ";
+                    cin >> k1;
+                    cout << "\nEnter key[2]: ";
+                    cin >> k2;
+                    cout << "\nEnter key[3]: ";
+                    cin >> k3;
+
+                    while(1)
+                    {
+                        if((k1 >= 1 && k1 <= 25) && (k2 >= 1 && k2 <= 25) && (k3 >= 1 && k3 <= 25))
+                        {
+                            /* Decrypt the final ciphertext using key k3*/
+                            string deinterText;
+                            deinterText = ceasarDecipher(finalCiphertext, k3);
+                            cout << "The Decrypted ciphertext: " << deinterText << "\n";
+
+                            /*Split the decrypted cipher into left and right ciphers
+                              Interleave the left and right ciphers and split again
+                              Decrypt left and right ciphers with keys k1 and k2 respectively
+                            */
+                            leftCi= leftSegment(deinterText);
+                            rightCi = rightSegment(deinterText);
+                            cout << "\nLeft Cipher: " << leftCi << "\n";
+                            cout << "Right Cipher: " << rightCi << "\n";
+
+                            string interCipher;
+                            interCipher = interLeavedCipher(leftCi, rightCi);
+
+                            string lDecipher;
+                            string rDecipher;
+                            lDecipher = leftSegment(interCipher);
+                            rDecipher = rightSegment(interCipher);
+                            leftSeg = leftDecipher(lDecipher, k1);
+                            rightSeg = rightDecipher(rDecipher, k2);
+
+                            cout << "\nOriginal Plaintext: " << leftSeg + rightSeg <<"\n";
+                            break; //return option input /outter loop
+                        }
+
+                        else
+                        {
+                            cout << "\nInvalid input(s)\n";
+                        }
+                    }
+                }
+
+                else
+                {
+                    cout << "Invalid option, exiting ...\n";
+                    break;
+                }
+                cout << "\nEnter your option: ";
+                cin >> option;
+                cin.ignore();
+                if(option >= 1  && option <= 3)
+                {
+                    if(option == 3)
+                    {
+                        cout << "Goodbye!\n";
+                        exit(1);
+                    }
+                }
+                else
+                {
+                    cout << "Invalid option, exiting ...\n";
+                    break;
+                }
+
+            }
         }
-        cout << "\nEnter your option: ";
-        cin >> option;
-        cin.ignore();
     }
     return 0;
 }
@@ -92,21 +191,17 @@ void mainMenu()
     cout <<"\t*****************************************************\n";
 }
 
-string plainText()
+/* Beginning of encryption block */
+
+string userInput()
 {
     string text;
-    cout << "\nEnter a plaintext: ";
     getline(cin, text);
 
     for(int i = 0; i < text.length(); i++)
     {
-        if(text[0] == '\0')
-        {
-            cout << "\nEnter a plaintext: ";
-            getline(cin, text);
-        }
-        else
-            text[i] = tolower(text[i]);
+        //convert input to lower case
+        text[i] = tolower(text[i]);
     }
     return text;
 }
@@ -216,6 +311,94 @@ string rightCipher(string &rightText, const int k2)
                 interCipher[i]++ ;
         }
     }
-    cout << "\nThe final Cipher text: " << interCipher << "\n";
     return interCipher;
 }
+
+/* End of encryption block */
+
+/* ***************************** */
+
+/* Beginning of decryption block */
+
+ string ceasarDecipher(string &finalCipher, const int k3)
+ {
+
+    int len = 0;
+    cout << "The key[k3]: " << k3 <<"\n";
+    cout << "The finalCipher: " << finalCipher <<"\n";
+
+    len = finalCipher.length();
+
+    for(int i = 0; i < len; i++)
+    {
+        for(int k = 0; k < k3; k++)
+        {
+            /*Restart the shifting from 'z' when at 'a'
+              Else shift back k3 positions to get the interleaved ciphertext
+            */
+            if(finalCipher[i] == 'a')
+                finalCipher[i] = 'z';
+            else
+                finalCipher[i]-- ;
+        }
+    }
+    return finalCipher;
+ }
+
+
+string leftDecipher(string &leftCi, const int k1)
+{
+    int len = 0;
+
+    cout << "The key[k1]: " << k1 <<"\n";
+    cout << "The left cipher: " << leftCi <<"\n";
+
+    len = leftCi.length();
+
+    for(int i = 0; i < len; i++)
+    {
+        for(int k = 0; k < k1; k++)
+        {
+            /*Restart the shifting from 'z' when at 'a'
+              Else continue shifting backwards k1 position
+            */
+            if(leftCi[i] == 'a')
+                leftCi[i] = 'z';
+            else
+                leftCi[i]-- ;
+           //cout << "The Left Cipher[" << i <<"]: " << leftText[i] << "\n";
+        }
+    }
+    cout << "The Left Text: " << leftCi << "\n";
+    return leftCi;
+}
+
+string rightDecipher(string &rightCi, const int k2)
+{
+    int len = 0;
+
+    cout << "The key[k2]: " << k2 <<"\n";
+    cout << "The right cipher: " << rightCi <<"\n";
+
+    len = rightCi.length();
+
+    for(int i = 0; i < len; i++)
+    {
+        for(int k = 0; k < k2; k++)
+        {
+            /*Restart the shifting from 'z' when at 'a'
+              Else continue shifting backwards k2 position
+            */
+            if(rightCi[i] == 'a')
+                rightCi[i] = 'z';
+            else
+                rightCi[i]-- ;
+           //cout << "The Left Cipher[" << i <<"]: " << leftText[i] << "\n";
+        }
+    }
+    cout << "The Right Text: " << rightCi << "\n";
+    return rightCi;
+}
+
+
+/* End of decryption block */
